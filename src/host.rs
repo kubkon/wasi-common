7d6945fac0d7bae41721b1aa3bdf187b471b9240
Dan Gohman <sunfish@mozilla.com>
2019-11-01 13:29:23 -0700

    Use pub(crate).

--- src/host.rs
+++ src/host.rs
@@ -11,56 +11,56 @@ pub(crate) type void = ::std::os::raw::c_void;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-pub struct __wasi_prestat_t {
-    pub pr_type: __wasi_preopentype_t,
-    pub u: __wasi_prestat_t___wasi_prestat_u,
+pub(crate) struct __wasi_prestat_t {
+    pub(crate) pr_type: __wasi_preopentype_t,
+    pub(crate) u: __wasi_prestat_t___wasi_prestat_u,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-pub union __wasi_prestat_t___wasi_prestat_u {
-    pub dir: __wasi_prestat_t___wasi_prestat_u___wasi_prestat_u_dir_t,
+pub(crate) union __wasi_prestat_t___wasi_prestat_u {
+    pub(crate) dir: __wasi_prestat_t___wasi_prestat_u___wasi_prestat_u_dir_t,
     _bindgen_union_align: u64,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct __wasi_prestat_t___wasi_prestat_u___wasi_prestat_u_dir_t {
-    pub pr_name_len: usize,
+pub(crate) struct __wasi_prestat_t___wasi_prestat_u___wasi_prestat_u_dir_t {
+    pub(crate) pr_name_len: usize,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct __wasi_ciovec_t {
-    pub buf: *const void,
-    pub buf_len: usize,
+pub(crate) struct __wasi_ciovec_t {
+    pub(crate) buf: *const void,
+    pub(crate) buf_len: usize,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct __wasi_iovec_t {
-    pub buf: *mut void,
-    pub buf_len: usize,
+pub(crate) struct __wasi_iovec_t {
+    pub(crate) buf: *mut void,
+    pub(crate) buf_len: usize,
 }
 
 #[allow(unused)]
-pub unsafe fn ciovec_to_host(ciovec: &__wasi_ciovec_t) -> io::IoSlice {
+pub(crate) unsafe fn ciovec_to_host(ciovec: &__wasi_ciovec_t) -> io::IoSlice {
     let slice = slice::from_raw_parts(ciovec.buf as *const u8, ciovec.buf_len);
     io::IoSlice::new(slice)
 }
 
 #[allow(unused)]
-pub unsafe fn ciovec_to_host_mut(ciovec: &mut __wasi_ciovec_t) -> io::IoSliceMut {
+pub(crate) unsafe fn ciovec_to_host_mut(ciovec: &mut __wasi_ciovec_t) -> io::IoSliceMut {
     let slice = slice::from_raw_parts_mut(ciovec.buf as *mut u8, ciovec.buf_len);
     io::IoSliceMut::new(slice)
 }
 
-pub unsafe fn iovec_to_host(iovec: &__wasi_iovec_t) -> io::IoSlice {
+pub(crate) unsafe fn iovec_to_host(iovec: &__wasi_iovec_t) -> io::IoSlice {
     let slice = slice::from_raw_parts(iovec.buf as *const u8, iovec.buf_len);
     io::IoSlice::new(slice)
 }
 
-pub unsafe fn iovec_to_host_mut(iovec: &mut __wasi_iovec_t) -> io::IoSliceMut {
+pub(crate) unsafe fn iovec_to_host_mut(iovec: &mut __wasi_iovec_t) -> io::IoSliceMut {
     let slice = slice::from_raw_parts_mut(iovec.buf as *mut u8, iovec.buf_len);
     io::IoSliceMut::new(slice)
 }
